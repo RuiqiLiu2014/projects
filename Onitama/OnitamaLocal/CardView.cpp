06d3b149027e54c5920118ca802df220596cfbe6
Ruiqi Liu <ruiqiliu2014@gmail.com>
2025-12-22 22:44:52 -0800

    display done

--- Onitama/OnitamaLocal/CardView.cpp
+++ Onitama/OnitamaLocal/CardView.cpp
@@ -10,13 +10,22 @@
 
 void CardView::draw() const
 {
+    const Rectangle border = {x - Display::CARD_WIDTH / 2, y - Display::CARD_HEIGHT / 2, Display::CARD_WIDTH, Display::CARD_HEIGHT};
+    DrawRectangleRoundedLinesEx(border, 0.2, 10, 1.0, BLACK);
+
     constexpr float size = Display::CARD_CELL_SIZE;
+
+    const char* text = card.name.c_str();
+    const int textWidth = MeasureText(text, Display::FONT_SIZE);
+    DrawText(text, x - textWidth / 2, y - Display::CARD_HEIGHT / 2 + 5, Display::FONT_SIZE, BLACK);
+
+    const float center_y = y + Display::CARD_HEIGHT / 2 - size * 3;
 
     for (int r = -2; r <= 2; r++)
     {
         for (int c = -2; c <= 2; c++)
         {
-            const Rectangle rect = { x - size / 2 + c * size, y - size / 2 + r * size, size, size };
+            const Rectangle rect = { x - size / 2 + c * size, center_y - size / 2 + r * size, size, size };
             if (r == 0 && c == 0)
             {
                 DrawRectangleRec(rect, DARKGRAY);
--- Onitama/OnitamaLocal/DisplayConfig.h
+++ Onitama/OnitamaLocal/DisplayConfig.h
@@ -13,7 +13,11 @@ namespace Display
     constexpr int WINDOW_HEIGHT = 1200;
 
     constexpr int CELL_SIZE = 100;
-    constexpr int CARD_CELL_SIZE = 40;
+
+    constexpr float CARD_WIDTH = 220;
+    constexpr float CARD_HEIGHT = 260;
+    constexpr float CARD_CELL_SIZE = 40;
+    constexpr int FONT_SIZE = 30;
 
     constexpr int PLAYER_VIEW_Y_OFFSET = 200;
     constexpr int MIDDLE_CARD_X_OFFSET = 200;
